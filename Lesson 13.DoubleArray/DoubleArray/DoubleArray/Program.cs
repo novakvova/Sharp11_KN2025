8657aef1ba31b3c3a859376aca76d1d71e31fb11
novakvova <novakvova@gmail.com>
2025-10-16 11:00:16 +0300

    Working Array

---
+++ Lesson 13.DoubleArray/DoubleArray/DoubleArray/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Привіт Козаки і Козачки :)");
+
+//Cтворюємо масив 2х2
+//Я маю масив, який склається з 2 рядків,
+//[] - не зрозуміло скільки стовпчиків
+int[][] a = new int[2][];
+a[0] = new int[2]; //У пешому рядку - 2 стовпчика
+a[1] = new int[2]; //У другому ряку - 2 стовпчика
+
+//Індексаці масиву починається з 0
+a[0][0] = 2;
+a[0][1] = 4;
+a[1][0] = 8;
+a[1][1] = 6;
+
+Console.Write($"{a[0][0]}\t");
+Console.Write($"{a[0][1]}\n");
+Console.Write($"{a[1][0]}\t");
+Console.Write($"{a[1][1]}\n");
+
+Console.WriteLine("\n\n--Вивід масиву через цикл--");
+int n = 2; //кілкість рядків
+int m = 2; //кількість стовпчиків
+for (int i = 0; i < n; i++)
+{
+    for(int j = 0; j < m; j++)
+    {
+        Console.Write($"{a[i][j]}\t");
+    }
+    Console.WriteLine();
+}    
---
+++ Lesson 14.TaskArray/TaskArray/TaskArray/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+
+int[] simple = { 4, 6, 12, 8 };
+//Console.WriteLine(simple);
+foreach (int item in simple)
+{
+    Console.Write($"{item}\t");
+}
+Console.WriteLine();
+
+int[,] a =
+{
+    { 3,  7,  12 },
+    { 4,  6,  18 },
+    { 45, -6, 87 }
+};
+int n = 3, m = 3;
+Console.WriteLine("----Matrix A----");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write($"{a[i,j]}\t");
+
+    Console.WriteLine();
+}
+
+
+
+//Console.WriteLine("Привіт масиви :)");
