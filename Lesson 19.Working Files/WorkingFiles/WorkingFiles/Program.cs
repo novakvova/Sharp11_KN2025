a69465bf73af21f40ec312cd703da0f09852425b
novakvova <novakvova@gmail.com>
2025-11-07 17:28:45 +0200

    working is good app

---
+++ Lesson 19.Working Files/WorkingFiles/WorkingFiles/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+//Console.WriteLine("Привіт команда");
+
+//Файл - це найменоване місце не диску для зберігання інформації
+//Дозволяє працювати із файлми
+//File - Для роботи з файлами
+//WriteAllText() - Це функція, яка приймає на себе назву файлу або
+//шлях і примає текст, який потрібно записати у файл.
+// -----------Запис даних у файл------------
+//string fileName = "myText.txt"; //Назва файлу, куди будемо писати інформацію
+//string text = "Пишому код і насолоджуємося цим процесом";
+//File.WriteAllText(fileName, text); //Виводимо у файл myText.txt - наш текст
+
+//-------------Читання даних із файлу.-------------------
+//string fileName = "myText.txt";
+//string text = File.ReadAllText(fileName); //повертає дані, які є у файлі
+//Console.WriteLine("----------Вміст файлу--------");
+//Console.WriteLine(text);
+
+
+int action = 0;
+
+do
+{
+    Console.WriteLine("1.Читання даних з файлу");
+    Console.WriteLine("2.Запис тексту у файл");
+    Console.Write("->_");
+    string text = Console.ReadLine();
+    int.TryParse(text, out action);
+    switch(action)
+    {
+        case 1:
+            {
+                Console.WriteLine("Вкажіть шлях або назву файлу:");
+                string fileName = Console.ReadLine();
+                string fileText = File.ReadAllText(fileName);
+                Console.WriteLine("---------Вміст файлу----------");
+                Console.WriteLine(fileText);
+                break;
+            }
+
+        case 2:
+            {
+                Console.WriteLine("Вкажіть шлях або назву файлу:");
+                string fileName = Console.ReadLine();
+                Console.WriteLine("Вкажіть вміст файлу");
+                string fileContent = Console.ReadLine();
+                File.WriteAllText(fileName, fileContent);
+                break;
+            }
+    }
+
+} while (action!=0);
+    
---
+++ Lesson 20.Directory, File, Enveroment/WorkingFileSystem/WorkingFileSystem/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+
+//Призначений для отримання інформації про ОС і диски на вашого ПК
+//Environment.CurrentDirectory
+//---------------------------Enveroment---------------------------------
+/*
+Console.WriteLine($"Ваша поточн папка {Environment.CurrentDirectory}");
+Console.WriteLine($"Інформація про ПК ядра {Environment.ProcessorCount}");
+Console.WriteLine($"Версія OS ядра {Environment.OSVersion}");
+
+string[] drives = Environment.GetLogicalDrives();
+Console.Write($"Диски на вашого ПК ");
+foreach (string drive in drives)
+    Console.Write($"{drive}, ");
+Console.WriteLine();
+*/
+
+//Клас для роботи з папками або каталогами
+//Список папок у поточній папці
+//path - шлях до папки 
+string path = "C:\\";
+var list = Directory.GetDirectories(path);
+Console.WriteLine($"Список папок по шляху {path}");
+foreach(var item in list)
+{
+    Console.WriteLine(item);
+}
+//Отримує інформацію про файли у папці
+var files = Directory.GetFiles(path);
+Console.WriteLine($"Файли у папці {files}");
+foreach (var file in files)
+{ 
+    Console.WriteLine(file); 
+}
+
+Console.WriteLine("Вкажіть назву папки:");
+string folderName = Console.ReadLine();
+
+string pathFolder = path + folderName; //Обєднуємо шлях із папкою
+if(Directory.Exists(pathFolder))
+{
+    Console.WriteLine("Дана папка існує :)");
+}
+else
+{
+    Console.WriteLine("Створити папку? так/ні");
+    string yes = Console.ReadLine();
+    if(yes != null) //не пустий рядок
+    {
+        if(yes.ToLower() == "так")
+        {
+            Directory.CreateDirectory(pathFolder);
+        }
+    }
+}
+
+
+
